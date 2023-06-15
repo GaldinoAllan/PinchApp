6e18f8b69fddfad6df7e4c092ec1595449674897
GaldinoAllan <galdinoallancodes@gmail.com>
2023-06-15 20:39:01 -0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Allan Galdino on 15/06/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
