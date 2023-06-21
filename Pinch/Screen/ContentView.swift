8e8425baf2ab7b08dff1021595e2b5d96ec82c68
GaldinoAllan <galdinoallancodes@gmail.com>
2023-06-20 21:22:16 -0300

    feature: SwiftUI Magnification Gesture

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -50,8 +50,27 @@ struct ContentView: View {
                                 if imageScale <= 1 {
                                     resetImageState()
                                 }
-                                
                             }
+                    )
+                // MARK: - Magnification
+                    .gesture(
+                        MagnificationGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    if imageScale >= 1 && imageScale <= 5 {
+                                        imageScale = value
+                                    } else if imageScale > 5 {
+                                        imageScale = 5
+                                    }
+                                }
+                            }
+                            .onEnded({ _ in
+                                if imageScale > 5 {
+                                    imageScale = 5
+                                } else if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            })
                     )
             } // ZStack
             .navigationTitle("Pinch & Zoom")
